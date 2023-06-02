e5c2945c289e2995df947518f52a3636282a0bbd
ivrequenajerez <ianrequenajerez@gmail.com>
2023-06-02 14:54:52 +0200

    -

--- myscript.js
+++ myscript.js
@@ -67,3 +67,23 @@ window.addEventListener("scroll", function() {
     header.classList.remove("scrolled");
   }
 });
+
+// Mostrar al hacer scroll
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal')
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
